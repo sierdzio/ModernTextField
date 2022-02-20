a92cdd879f382a44c3398e54950bf49a79e5594b
Tomasz Siekierda <sierdzio@gmail.com>
2022-02-20 15:58:36 +0100

    Add block-based painting, woohoo!

--- painter.cpp
+++ painter.cpp
@@ -5,15 +5,64 @@
 
 #include <QDebug>
 
+QPair<Block, Block> Block::split(const int where) const
+{
+    Block first(*this), second(*this);
+
+    const int index = where * text.length() / size().width();
+
+    first.text = text.left(index);
+    first.compute();
+    second.text = text.mid(index);
+    second.compute();
+
+    return { first, second };
+}
+
+QSize Block::size() const
+{
+    return _size;
+}
+
+QFont Block::font() const
+{
+    // TODO: custom font...
+    QFont font;
+
+    if (types.testFlag(Type::Bold)) {
+        font.setBold(true);
+    }
+
+    if (types.testFlag(Type::Italic)) {
+        font.setItalic(true);
+    }
+
+    return font;
+}
+
+void Block::compute()
+{
+    if (types.testFlag(Type::Text)) {
+        const QFontMetrics metrics(font());
+
+        _size = metrics.boundingRect(0, 0, 1000, 1000, Qt::AlignLeft | Qt::AlignTop, text).size();
+    }
+}
+
 Painter::Painter(QObject *parent) : QObject(parent)
 {
+    _blocks = {
+        { Block::Type::Text, "Hello world!" },
+        { Block::Type::Text | Block::Type::Bold, "Bold world, too!" },
+        { Block::Type::Text | Block::Type::Italic, "Here, have some italics, mate." }
+    };
 }
 
 void Painter::paint(QPainter *painter)
 {
     painter->save();
 
-    const QString text("Hello world!");
+//    const QString text("Hello world!");
 //    const auto metrics = painter->fontMetrics().boundingRect(text);
 
     const auto mainRect = QRect(0, 0, _size.width(), _size.height());
@@ -21,7 +70,45 @@ void Painter::paint(QPainter *painter)
 //    painter->drawText(QPoint((_size.width() - metrics.width()) * .5,
 //                             (_size.height() - metrics.height()) * .5),
 //                      text);
-    painter->drawText(mainRect, _alignment, text);
+
+    int line = 0;
+    int column = 0;
+    for (qsizetype i = 0; i < _blocks.size(); ++i) {
+        const auto &block = _blocks.at(i);
+
+        if (column + 5 >= mainRect.width()) {
+            // Line break!
+            line += block.size().height();
+            column = 0;
+        }
+
+        if (column + block.size().width() > mainRect.width()) {
+            const auto chunks = block.split(mainRect.width() - column);
+
+            _blocks.replace(i, chunks.first);
+            _blocks.insert(i + 1, chunks.second);
+
+            painter->setFont(chunks.first.font());
+
+            painter->drawText(column, line,
+                              mainRect.width() - column, mainRect.height() - line,
+                              _alignment, chunks.first.text);
+
+
+            // Line break!
+            line += block.size().height();
+            column = 0;
+            continue;
+        }
+
+        painter->setFont(block.font());
+
+        painter->drawText(column, line,
+                          mainRect.width() - column, mainRect.height() - line,
+                          _alignment, block.text);
+
+        column += block.size().width();
+    }
 
     painter->restore();
 }
--- painter.h
+++ painter.h
@@ -3,6 +3,52 @@
 
 #include <QObject>
 #include <QSize>
+#include <QUrl>
+#include <QFlag>
+
+struct Block {
+    Q_GADGET
+
+public:
+    enum Type {
+        None = 0x0000,
+        Text = 0x0001,
+        Link = 0x0002,
+        Bold = 0x0004,
+        Italic = 0x0008,
+        Code = 0x0010,
+        Citation = 0x0020,
+        Emoji = 0x0040,
+        CustomEmoji = 0x0080,
+        Strikethrough = 0x0100,
+        Underline = 0x0200
+    };
+    Q_DECLARE_FLAGS(Types, Type)
+    Q_FLAG(Types)
+
+    Types types = Type::None;
+    bool showThumbnail = true;
+
+    QUrl linkDestination;
+    QString text;
+
+    QPair<Block, Block> split(const int where) const;
+
+    QSize size() const;
+    QFont font() const;
+
+    Block (const Types types_, const QString &text_) : types(types_), text(text_)
+    {
+        compute();
+    }
+
+private:
+    void compute();
+
+    QSize _size;
+};
+
+ Q_DECLARE_OPERATORS_FOR_FLAGS(Block::Types)
 
 class QPainter;
 
@@ -27,9 +73,15 @@ public:
     const Qt::Alignment &alignment() const;
     void setAlignment(const Qt::Alignment &newAlignment);
 
+    bool addText(const Block &block);
+    bool removeCharacter(const int index = -1);
+    bool removeString(const int from, const int to = -1);
+
 private:
     QSize _size = QSize(300, 200);
     Qt::Alignment _alignment = Qt::AlignmentFlag::AlignLeft | Qt::AlignmentFlag::AlignTop;
+
+    QList<Block> _blocks;
 };
 
 #endif // PAINTER_H
